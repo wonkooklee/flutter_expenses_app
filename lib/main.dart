a6efc3e3e535b44fb3b18cc43093e44b4042a2e6
Wonkook Lee <wonkook.lee@medistream.com>
2022-02-01 02:12:29 +0900

    Styling a Container

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,17 @@ class MyHomePage extends StatelessWidget {
                 child: Row(
                   children: <Widget>[
                     Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
                       child: Text(
                         tx.amount.toString(),
                       ),
@@ -61,7 +72,9 @@ class MyHomePage extends StatelessWidget {
                     Column(
                       children: <Widget>[
                         Text(tx.title),
-                        Text(tx.date.toString()),
+                        Text(
+                          tx.date.toString(),
+                        ),
                       ],
                     )
                   ],
